f55ae050213bbc0d54f1a82f614d4e7c83166917
Ketan Modi <ketanmodi@kanhasofttech.com>
2023-12-22 17:29:08 +0530

    sidebar logout and dashboard cards update

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,5 +1,5 @@
-import { SidebarRoutes } from './sidebar-routes'
-import { Logo } from './logo'
+import { SidebarRoutes } from "./sidebar-routes";
+import { Logo } from "./logo";
 
 export const Sidebar = () => {
   return (
@@ -7,9 +7,16 @@ export const Sidebar = () => {
       <div className="p-6">
         <Logo />
       </div>
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col w-full h-full">
         <SidebarRoutes />
+
+        {/* ------------ Logout Button ------------ */}
+        <div className="flex justify-center align-middle mt-auto mb-4">
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Logout
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/admin-access/dashboard/dashboard-page.jsx
+++ src/pages/admin-access/dashboard/dashboard-page.jsx
@@ -1,8 +1,60 @@
-
 const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
-}
+  // Sample array of card data
+  const cardDataArray = [
+    {
+      id: 1,
+      imgSrc: 'path/to/image1.jpg',
+      heading: 'Card 1 Heading',
+      paragraph: 'This is card 1.',
+    },
+    {
+      id: 2,
+      imgSrc: 'path/to/image2.jpg',
+      heading: 'Card 2 Heading',
+      paragraph: 'This is card 2.',
+    },
+    {
+      id: 3,
+      imgSrc: 'path/to/image2.jpg',
+      heading: 'Card 2 Heading',
+      paragraph: 'This is card 2.',
+    },
+    {
+      id: 4,
+      imgSrc: 'path/to/image2.jpg',
+      heading: 'Card 2 Heading',
+      paragraph: 'This is card 2.',
+    },
+    {
+      id: 5,
+      imgSrc: 'path/to/image2.jpg',
+      heading: 'Card 2 Heading',
+      paragraph: 'This is card 2.',
+    },
+    {
+      id: 6,
+      imgSrc: 'path/to/image2.jpg',
+      heading: 'Card 2 Heading',
+      paragraph: 'This is card 2.',
+    },
+    // Add more card objects as needed
+  ];
 
-export default DashboardPage
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        {cardDataArray.map((card) => (
+          <div key={card.id} className="max-w-md rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={card.imgSrc} alt={`Card ${card.id} Image`} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{card.heading}</div>
+              <p className="text-gray-700 text-base">{card.paragraph}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
