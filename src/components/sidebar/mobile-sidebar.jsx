8483e8356a8705fb9a50540e805a90da1cd72fa7
Ketan Modi <ketanmodi@kanhasofttech.com>
2024-01-01 11:21:06 +0530

    code commited

--- src/components/sidebar/mobile-sidebar.jsx
+++ src/components/sidebar/mobile-sidebar.jsx
@@ -1,30 +1,3 @@
-// // MobileMenu.js
-
-// import { useNavigate } from "react-router-dom";
-// import { Menu } from "lucide-react";
-// import { Sidebar } from "./sidebar";
-
-// export const MobileSideBar = ({ onToggle }) => {
-//   const navigate = useNavigate();
-
-//   const handleClick = () => {
-//     onToggle();
-//     navigate("/");
-//   };
-
-//   return (
-//     <>
-//       <button
-//         onClick={handleClick}
-//         type="button"
-//         className="md:hidden px-4 py-2 text-slate-500"
-//       >
-//         <Menu size={24} />
-//         {/* <Sidebar /> */}
-//       </button>
-//     </>
-//   );
-// };
 import { useState } from "react";
 import { Menu } from "lucide-react";
 import { Sidebar } from "./sidebar";
